bfd5f952468e7f0c2a383f4fcb7ce754fb5601f0
zebias <729976854@qq.com>
2018-06-08 20:53:40 +0800

    update
    简直6666

---
+++ getData.py
@@ -0,0 +1,17 @@
+import sqlite3
+conn = sqlite3.connect('test.db')
+cursor = conn.cursor()
+cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+cursor.execute('insert into user (id, name) values (\'1\', \'Michael\')')
+cursor.rowcount
+cursor.close()
+conn.commit()
+conn.close()
+
+
+cursor.execute('select * from user where id=?', ('1',))
+
+values = cursor.fetchall()
+values
+cursor.close()
+conn.close()
